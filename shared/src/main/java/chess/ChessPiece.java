033a1f280c9adfda898d9b10285b236a68fd8900
adam-clark-git <adam.b.clark@icloud.com>
2026-09-14 19:44:35 -0600

    Implemented hashCode for Pieces

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -52,6 +53,10 @@ public class ChessPiece {
             sPiece = sPiece.toUpperCase();
         }
         return sPiece;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 
     /**
